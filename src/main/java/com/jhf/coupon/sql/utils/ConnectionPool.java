2912860d6a617e98382796bdeeb7aa15316d873b
Khaled Salhab <khaled.salhab@forescout.com>
2021-12-18 07:24:09 +0200

    added mySQL maven dependency in pom.xml added Part 3 (ConnectionPool)

---
+++ src/main/java/com/jhf/coupon/sql/utils/ConnectionPool.java
@@ -0,0 +1,68 @@
+package com.jhf.coupon.sql.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+class ConnectionPool {
+	private static final String URL = "jdbc:mysql://localhost:3306/jhf/couponSystem";
+	private static final String USER = "projectUser";
+	private static final String PASSWORD = "projectUser";
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+	private static final Set<Connection> connections = new HashSet<Connection>();
+	private static ConnectionPool instance = null;
+
+
+	private ConnectionPool() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			for (int i = 10; i > 0; i--) {
+				connections.add(DriverManager.getConnection(URL, USER, PASSWORD));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ConnectionPool getInstance() {
+		if (instance == null) {
+			instance = new ConnectionPool();
+		}
+		return instance;
+	}
+
+	public Connection getConnection() throws InterruptedException {
+		Connection connection = null;
+		if (connections.isEmpty()) {
+			wait();
+		} else {
+			Iterator<Connection> it = connections.iterator();
+			connection = it.next();
+			connections.remove(connection);
+		}
+		return connection;
+
+	}
+
+	public static void restoreConnection(Connection connection) {
+		connections.add(connection);
+	}
+
+	public static void closeAll() {
+		Iterator<Connection> iterator = connections.iterator();
+		while (iterator.hasNext() & iterator.next() != null) {
+			try {
+				iterator.next().close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
