234c078f41aa54e4742d75b1414a23a19b2d49dc
Khaled Salhab <khaled.salhab@forescout.com>
2022-01-14 14:38:43 +0200

    DailyJob , Program & Test //todo implement Test & DailyJob

---
+++ src/main/java/com/jhf/coupon/Program.java
@@ -0,0 +1,7 @@
+package com.jhf.coupon;
+
+public class Program {
+	public static void main(String[] args) {
+		Test.testAll();
+	}
+}
---
+++ src/main/java/com/jhf/coupon/Test.java
@@ -0,0 +1,8 @@
+package com.jhf.coupon;
+
+public class Test {
+	public static void testAll() {
+		//todo add testing here
+
+	}
+}
